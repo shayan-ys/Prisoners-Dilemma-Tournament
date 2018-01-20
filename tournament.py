0d97078a33695da1c8918fd133bc57ab1ffbbdd3
Shayan Yousefian <shayan.yousefian1372@gmail.com>
2018-01-20 18:14:33 -0500

    Evolution tournament added

--- tournament.py
+++ tournament.py
@@ -96,24 +96,36 @@ def game(a: Prisoner, b: Prisoner):
 
 class Tournament:
     population = []
+    pop_seed = {}
     pop_len = 0
     evo_members_replacement_count = 3
-    evo_games_ratio = 0
+    evo_games_ratio = 100
 
     def __init__(self, *args, **kwargs):
         self.population = []
 
+        if 'evo_games_ratio' in kwargs:
+            self.evo_games_ratio = int(kwargs['evo_games_ratio'])
+
         if 'seed_counts' in kwargs:
             self.seed(kwargs['seed_counts'])
 
-    def seed(self, seed_counts: dict):
+    def seed(self, seed_counts: dict=None):
+        if not seed_counts:
+            seed_counts = self.pop_seed
         for prisoner_type, count in seed_counts.items():
             for i in range(count):
                 self.population.append(prisoner_type())
+        self.pop_seed = seed_counts
         self.pop_len = len(self.population)
+        self.evo_games_ratio = max(1, int(self.evo_games_ratio / self.pop_len) * self.pop_len)
 
     def play_next(self, play_index: int=0):
         if play_index and not play_index % self.pop_len:
+            if not play_index % self.evo_games_ratio:
+                # print(str(play_index) + ' % ' + str(self.evo_games_ratio) + ' = 0')
+                if not self.evolution():
+                    return False
             shuffle(self.population)
 
         player_1 = self.population.pop(0)
@@ -124,14 +136,17 @@ class Tournament:
         self.population.append(player_1)
         self.population.append(player_2)
 
-    def find_extreme_members(self, given_arr):
+        return True
+
+    def find_extreme_members(self) -> (list, list):
         best_members = []
         worst_members = []
 
-        for indv in given_arr:
+        for indv in self.population:
+            indv = indv   # type: Prisoner
             len_best_members = len(best_members)
 
-            if len_best_members == self.evo_members_replacement_count and indv > best_members[0]:
+            if len_best_members == self.evo_members_replacement_count and indv.score > best_members[0].score:
                 best_members.pop(0)
                 len_best_members -= 1
 
@@ -140,7 +155,7 @@ class Tournament:
                 insert_point = 0
                 add_before = False
                 while insert_point < len(best_members):
-                    if indv < best_members[insert_point]:
+                    if indv.score < best_members[insert_point].score:
                         add_before = True
                         break
                     insert_point += 1
@@ -151,7 +166,7 @@ class Tournament:
 
             len_worst_members = len(worst_members)
 
-            if len_worst_members == self.evo_members_replacement_count and indv < worst_members[0]:
+            if len_worst_members == self.evo_members_replacement_count and indv.score < worst_members[0].score:
                 worst_members.pop(0)
                 len_worst_members -= 1
 
@@ -160,7 +175,7 @@ class Tournament:
                 insert_point = 0
                 add_before = False
                 while insert_point < len(worst_members):
-                    if indv > worst_members[insert_point]:
+                    if indv.score > worst_members[insert_point].score:
                         add_before = True
                         break
                     insert_point += 1
@@ -169,24 +184,39 @@ class Tournament:
                 else:
                     worst_members.append(indv)
 
-            print(indv)
-            print(worst_members)
-            print(best_members)
+        return best_members, worst_members
 
-    def evolution(self):
+    def evolution(self) -> bool:
         # find top for e.g. 3
         # find worst 3
+        best_mems, worst_mems = self.find_extreme_members()
         # new members, same types as top 3
-        pass
+        for member in best_mems:
+            self.pop_seed[type(member)] += 1
+        # deleting members, same as worst 3
+        for member in worst_mems:
+            self.pop_seed[type(member)] -= 1
+        self.population = []
+        self.seed()
+
+        for prisoner_type, type_count in self.pop_seed.items():
+            if type_count == self.pop_len:
+                print(str(prisoner_type) + ' dominated the population !')
+                return False
+        return True
+
+    def print_report(self):
+        for prisoner_type, type_count in self.pop_seed.items():
+            print(str(prisoner_type) + ': ' + str(type_count))
 
 
 print("Hello world!")
 
 pop_seed_counts = {
-    PrisonerCoOp: 1,
-    PrisonerDefect: 1,
-    PrisonerCoinFlip: 1,
-    PrisonerTitForTat: 2
+    PrisonerCoOp: 10,
+    PrisonerDefect: 40,
+    PrisonerCoinFlip: 10,
+    PrisonerTitForTat: 40
 }
 
 # benchmark_runs = 100000
@@ -203,9 +233,12 @@ pop_seed_counts = {
 tour = Tournament()
 tour.seed(pop_seed_counts)
 
-tour.find_extreme_members([6, 2, 4, 7, 9, 3, 11, 8])
+# tour.find_extreme_members([6, 2, 4, 7, 9, 3, 11, 8])
 
 print(tour.population)
-for i in range(10):
-    tour.play_next(i)
+for i in range(1000000):
+    if not tour.play_next(i):
+        break
 print(tour.population)
+# print(len(tour.population))
+print(tour.print_report())
